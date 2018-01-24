70d6251ba9160da921c2a73fb7249f567e770e57
abyss <roger_ren@qq.com>
2018-01-24 14:19:55 +0800

    :tada: 修改host 0.0.0.0

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const WebSocket = require('ws');
+const http = require('http');
 
-const wss = new WebSocket.Server({ port: 9999 });
+// const wss = new WebSocket.Server({ port: 9999 });
+const server = http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Hello World\n'); }).listen(9999, '0.0.0.0');
+const wss = new WebSocket.Server({ server: server});
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
@@ -14,5 +17,5 @@ wss.on('connection', function connection(ws) {
 const ws = new WebSocket('ws://localhost:9999');
 
 ws.on('open', function open() {
-    ws.send('苗总是傻逼');
+    ws.send('来自服务端的测试消息 from ws://0.0.0.0:9999/');
 });
