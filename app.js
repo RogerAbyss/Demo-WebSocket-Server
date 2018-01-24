43e270831d4c28a86e9a127e95f0fb6140488298
abyss <roger_ren@qq.com>
2018-01-24 16:50:21 +0800

    :tada: 处理message,disconnect

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-// const wss = new WebSocket.Server({ port: 9999 });
-const server = http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Hello World\n'); }).listen(9999, '0.0.0.0');
+const server = http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Websocket Server Success\n'); }).listen(9999, '0.0.0.0');
 const wss = new WebSocket.Server({ server: server});
 
-wss.on('connection', function connection(ws) {
+/**
+ * Connection
+ */
+
+wss.on('connection', function connection(ws, req) {
+    var ip = req.connection.remoteAddress
+
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+        var log = 'Received: ' + message
+        console.log(log);
+        console.dir(req)
+        ws.send(log);
     });
 
-    ws.send('something');
+    ws.send('%s,连接成功!',ip);
 });
 
+/**
+ * DisConnection
+ */
+
+wss.on('disconnect', function connection(ws, req) {
+    var ip = req.connection.remoteAddress
+    ws.send('%s,断开了连接!',ip);
+});
+
+/**
+ * Error
+ */
+
+wss.on('error', function error(error) {
+    console.log(error)
+});
+
+/**
+ * 服务端Websoket 消息测试
+ * @type {*|WebSocket}
+ */
 
 const ws = new WebSocket('ws://localhost:9999');
 
 ws.on('open', function open() {
-    ws.send('来自服务端的测试消息 from ws://0.0.0.0:9999/');
+    ws.send('Start Success!');
 });
